a67dc534e0599a7c7e87198cc4a07308b9216533
edward-burn <9583964+edward-burn@users.noreply.github.com>
2020-03-20 14:12:39 +0100

    Update CodeToRun.R

--- Feasibility/Cohort Diagnostics/ECMO cohort diagnostics/extras/CodeToRun.R
+++ Feasibility/Cohort Diagnostics/ECMO cohort diagnostics/extras/CodeToRun.R
@@ -40,28 +40,15 @@ runCohortDiagnostics(connectionDetails = connectionDetails,
                      databaseName = databaseName,
                      databaseDescription = databaseDescription,
                      createCohorts = TRUE,
-                     runInclusionStatistics = FALSE,
-                     runIncludedSourceConcepts = FALSE,
-                     runOrphanConcepts = FALSE, 
-                     runTimeDistributions = FALSE,
-                     runBreakdownIndexEvents = FALSE,
-                     runIncidenceRates = FALSE,
-                     runCohortOverlap = FALSE,
-                     runCohortCharacterization = FALSE,
-                     minCellCount = 0)
+                     runInclusionStatistics = TRUE,
+                     runIncludedSourceConcepts = TRUE,
+                     runOrphanConcepts = TRUE, 
+                     runTimeDistributions = TRUE,
+                     runBreakdownIndexEvents = TRUE,
+                     runIncidenceRates = TRUE,
+                     runCohortOverlap = TRUE,
+                     runCohortCharacterization = TRUE,
+                     minCellCount = 5)
 
 # To view the results:
-# Optional: if there are results zip files from multiple sites in a folder, this merges them, which will speed up starting the viewer:
-CohortDiagnostics::preMergeDiagnosticsFiles(file.path(outputFolder, "diagnosticsExport"))
-
-# Use this to view the results. Multiple zip files can be in the same folder. If the files were pre-merged, this is automatically detected: 
 CohortDiagnostics::launchDiagnosticsExplorer(file.path(outputFolder, "diagnosticsExport"))
-
-
-# To explore a specific cohort in the local database, viewing patient profiles:
-CohortDiagnostics::launchCohortExplorer(connectionDetails = connectionDetails,
-                                        cdmDatabaseSchema = cdmDatabaseSchema,
-                                        cohortDatabaseSchema = cohortDatabaseSchema,
-                                        cohortTable = cohortTable,
-                                        cohortId = 123)
-# Where 123 is the ID of the cohort you wish to inspect.
